dea0c20049eb9bcd966719b1590ff56d68523546
ridhukhan <prakritirshandhane@gmail.com>
2025-12-17 13:45:42 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+require("dotenv").config()
+
+
+const express=require("express")
+const app =express()
+const mongoose=require("mongoose")
+const cors =require("cors")
+PORT =3000
+//middleware
+app.use(cors({origin: "https://naturalhealth.netlify.app"}))
+app.use(express.json())
+
+//mongoose atlas connect
+const  connectDB= async ()=>{
+    try {
+ await  mongoose.connect(process.env.MONGO_URL)
+        console.log("db is connect")
+    } catch (error) {
+        console.log("db is not connected")
+    }
+}
+
+//Schema + model
+const orderSchema = new mongoose.Schema({
+    name :{
+        type: String,
+        required: true
+    },
+     phone :{
+        type: String,
+        required: true
+    },
+     address :{
+        type: String,
+        required: true
+    }
+});
+
+const Order = mongoose.model("Order" ,orderSchema)
+
+
+app.post("/order", async (req,res)=>{
+const {name,phone,address}=req.body
+if(!name || !phone || !address){ 
+
+    return res.status(400).json({
+        message: "please fill all field"
+    })
+}
+const newOrder=new Order({
+    name,
+    phone,
+    address
+});
+await newOrder.save()
+res.status(201).json({
+    message:"order done"
+})
+})
+
+
+
+
+
+app.listen(PORT, async ()=>{
+
+console.log(`your app is runnig at http://localhost:${PORT}`)
+await connectDB()
+})
